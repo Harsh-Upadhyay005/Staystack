42a1b8553e8f18447aa6634a5809bd1c083b4ecf
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-03 20:12:24 +0530

    Add initial project structure with models, routes, and sample data
    - Created .gitignore to exclude unnecessary files
    - Implemented Listing model with schema
    - Set up Express server with MongoDB connection
    - Added sample listings data for initialization
    - Created views for displaying listings

---
+++ Models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    location: {
+        type: String,
+    }, 
+    imageUrl: {
+        type: String,
+        default: "https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-with-rocks-in-the-foreground-Md_-qx-b0-Q",
+        set(v) {
+            return v === "" 
+            ? "https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-with-rocks-in-the-foreground-Md_-qx-b0-Q" : v;
+        },
+
+    },
+    country: {
+        type: String,
+    },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require ("./Models/listing.js");
+const path = require ("path");
+
+app.set ("view engine", "ejs");
+app.set ("views", path.join (__dirname, "Views"));
+
+const MONGO_URL ='mongodb://127.0.0.1:27017/wanderlust';
+
+main().then(() => {
+    console.log("connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+
+})
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get ("/", (req, res) => {
+    res.send ("Hello World");
+});
+
+// index route to display all listings
+app.get ("/listings", async (req, res) => {
+    let listings = await Listing.find({});
+    res.render("listings/index.ejs", {listings: listings});
+});
+
+
+
+// app.get ("/testlisting", async (req, res) => {
+//     let sampleListing = new Listing ({
+//         title: "Beautiful Beach House",
+//         description: "A lovely beach house with stunning ocean views.", 
+//         price: 5250,
+//         location: "Malibu, CA",
+//         imageUrl: "",
+//         country: "USA"
+//     });
+
+//     await sampleListing.save().then(() => {
+//         res.send("Sample listing saved to database");
+//     }
+//     ).catch((err) => {
+//         res.status(500).send("Error saving listing: " + err);
+//     });
+
+// });
+
+app.listen(8080, () => {
+    console.log ("sever is running on port 8080");
+});
